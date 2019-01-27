0615aa408c5262deab03d80f64f4dce3f617559f
HughParsonage <hugh.parsonage@gmail.com>
2019-01-27 22:15:11 +1100

    Travis (#1)

--- travis-make.R
+++ travis-make.R
@@ -1,34 +1,62 @@
 
 
-# First remove all but the CGT_
+# First remove all but the CGT_parent
 vapply(dir(path = ".", pattern = "\\.tex$"), function(x) {
-  grepl("parent", x) && file.remove(x)
+  !grepl("parent", x) && file.remove(x)
 }, FALSE)
 
-install.packages("hutils", repos = "https://cran.rstudio.com", dependencies = FALSE, quiet = TRUE)
-install.packages("devtools", repos = "https://cran.rstudio.com", dependencies = TRUE, quiet = TRUE)
-install.packages("grattan", repos = "https://cran.rstudio.com", dependencies = FALSE, quiet = TRUE)
-install.packages("TeXCheckR", repos = "https://cran.rstudio.com", dependencies = TRUE)
+cat(as.character(Sys.time()), "\t", "hutils\n")
+install.packages("hutils", repos = "https://cran.rstudio.com", quiet = TRUE)
+cat(as.character(Sys.time()), "\t", "grattan\n")
+install.packages("grattan", repos = "https://cran.rstudio.com", quiet = TRUE)
+cat(as.character(Sys.time()), "\t", "TeXCheckR\n")
+install.packages("TeXCheckR", repos = "https://cran.rstudio.com", quiet = TRUE)
 
-devtools::install_github(paste0("hughparsonage/", c("hutilscpp", "grattanCharts")), 
+cat(as.character(Sys.time()), "\t", "hutilscpp\tgrattanCharts\n")
+devtools::install_github(paste0("hughparsonage/", 
+                                c("hutilscpp",
+                                  # "hildaExtra",
+                                  "grattanCharts")), 
                          quick = TRUE,
                          quiet = TRUE)
-
-install.packages(c("taxstats", "taxstats1516"),
-                 repos = "https://hughparsonage.github.io/drat",
-                 type = "source")
+cat(as.character(Sys.time()), "\t", "taxstats\n")
+if (!requireNamespace("taxstats", quietly = TRUE) || 
+    !requireNamespace("taxstats1516", quietly = TRUE)) {
+  install.packages(c("taxstats", "taxstats1516"),
+                   repos = "https://hughparsonage.github.io/tax-drat",
+                   quiet = TRUE,
+                   type = "source")
+}
 
 packages_ <-
-  c("bindrcpp", "hildaData", "hutils", "ggrepel", "testthat", 
-    "magrittr", "tidyr", "usethis", "devtools", "expm", "Hmisc", 
+  c("bindrcpp", "hutils", "ggrepel", "testthat", 
+    "magrittr", "tidyr", "usethis", "devtools", "expm", "profmem",
     "Formula", "lattice", "foreign", "survey", "survival", "Matrix", 
     "grid", "zoo", "httr", "rsdmx", "readr", "openxlsx", "readxl", 
     "xtable", "grattan", "directlabels", "scales", "ggplot2", "gridExtra", 
-    "dplyr", "haven", "devEMF", "knitr", 
+    "dplyr", "haven", "knitr", 
+    "lubridate",
+    "showtext", "sysfonts",
     "data.table", "sessioninfo")
-install.packages(packages_, repos = "https://cran.rstudio.com", dependencies = TRUE)
 
-knitr::knit("CGT_and_neg_gearing_parent.Rnw")
+# Remove already installed packages
+packages_to_install <- packages_
+for (i in seq_along(packages_)) {
+  packages_to_install[i] <- if (!length(find.package(packages_[i], quiet = TRUE))) "" else "(installed)"
+}
+packages_ <- packages_[packages_to_install == ""]
+
+cat("packages_\t", paste0(packages_, collapse = " "), "\n")
+if (!requireNamespace("showtext", quietly = TRUE)) {
+  install.packages("showtext", quiet = TRUE)
+}
+
+if (length(packages_) > 0) {
+  install.packages(packages_, repos = "https://cran.rstudio.com", quiet = TRUE)
+}
+
+# Wait until we work out hilda
+# knitr::knit("CGT_and_neg_gearing_parent.Rnw")
 
 
 
