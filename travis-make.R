d052dd372df12ca43facdddf2dc310a607de1240
HughParsonage <hugh.parsonage@gmail.com>
2019-02-11 15:51:34 +1100

    Hush, little Travis

--- travis-make.R
+++ travis-make.R
@@ -1,7 +1,7 @@
 
-if (!requireNamespace("devtools") ||
-    !requireNamespace("hunspell") || 
-    !requireNamespace("haven")) {
+if (!requireNamespace("devtools", quietly = TRUE) ||
+    !requireNamespace("hunspell", quietly = TRUE) || 
+    !requireNamespace("haven", quietly = TRUE)) {
   system("sudo apt-get install -y r-cran-devtools r-cran-rcpp r-cran-bh r-cran-stringi r-cran-hunspell r-cran-survey r-cran-dplyr r-cran-tidyr r-cran-readr r-cran-ggplot2 r-cran-ggrepel r-cran-haven r-cran-survival")
 }
 
